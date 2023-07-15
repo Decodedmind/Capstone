bdd59d916271771bf9e79d0ddde6f552a81af1f0
Decodedmind <115696970+Decodedmind@users.noreply.github.com>
2023-07-15 09:45:06 -0500

    info update

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from mcewens.views import hello_world
+from mcewens.views import hello_world, index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Mcewens/", hello_world, name="hello_world"),
+    path("", index, name="index"),
 ]
