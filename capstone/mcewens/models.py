0553570065ad6435dfda0e5569bc9ba127610594
Conner <cgray@basecampcodingacademy.org>
2023-07-18 21:04:01 -0500

    Created basic model, still need functions

--- capstone/mcewens/models.py
+++ capstone/mcewens/models.py
@@ -1,13 +1,48 @@
 from django.db import models
 
-# Create your models here.
 
 # one model - menu item
-# Name
-# Category - Lunch/ Dinner / Desert / Wine
-# Possibility of making exclusive toggle for ease of use? If Lunch, not WIne? Research?
-# Description
-# Price
-# Price percentage option?
-# Current item?
+class MenuItem(models.Model):
+    # Name - check
+    name = models.CharField(max_length=50)
+
+    # Category - Lunch/ Dinner / Desert / Wine - check
+    CATEGORY_CHOICES = (
+        ("Unselected", "Unselected"),
+        ("Lunch", "Lunch"),
+        ("Dinner", "Dinner"),
+        ("Dessert", "Dessert"),
+        ("Wine", "Wine"),
+    )
+
+    category = models.CharField(
+        choices=CATEGORY_CHOICES, default="Unselected", max_length=50, blank=True
+    )
+    # Possibility of making exclusive toggle for ease of use? If Lunch, not WIne? Research?
+
+    # Description - check
+    description = models.CharField(
+        max_length=50, blank=True, null=True
+    )  # should null be true?
+
+    # Price - check
+    price = models.FloatField()
+    # Price percentage option?
+
+    # Current item? check
+    current = models.IntegerField(default=1)
+
+
+def create_menu_item(name, category, description, price, current):
+    item = MenuItem(
+        name=name,
+        category=category,
+        description=description,
+        price=price,
+        current=current,
+    )
+    item.save()
+    return item
+
+
 # Submit button for forms
