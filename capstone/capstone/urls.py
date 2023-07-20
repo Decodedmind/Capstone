1e5932d297591879ccc9fe00430799698e5071ba
Conner <cgray@basecampcodingacademy.org>
2023-07-20 18:42:19 -0500

    Linked model to front end

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from mcewens.views import hello_world, index
+from mcewens.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Mcewens/", hello_world, name="hello_world"),
     path("", index, name="index"),
+    path("test/", create_menu_item_view, name="test"),
 ]
--- capstone/mcewens/views.py
+++ capstone/mcewens/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .forms import MenuItemForm
 
 
 def hello_world(request):
@@ -9,3 +10,25 @@ def hello_world(request):
 
 def index(request):
     return render(request, "index.html")
+
+
+# @login_required
+def create_menu_item_view(request):
+    if request.method == "POST":
+        # Create object of form
+        form = MenuItemForm(request.POST)
+        item = form.save(commit=False)
+        if form.is_valid():
+            context = {
+                "id": item.id,
+                "name": item.name,
+                "price": item.price,
+                "description": item.description,
+                "category": item.category,
+                "current": item.current,
+                "form": form,
+            }
+        return render(request, "test.html", context)
+    else:
+        form = MenuItemForm()
+        return render(request, "test.html", {"form": form})
