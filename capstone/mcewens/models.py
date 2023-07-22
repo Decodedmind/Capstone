3b5b8b1e285457b0299572a31626c0b981354a4b
Decodedmind <115696970+Decodedmind@users.noreply.github.com>
2023-07-22 09:04:06 -0500

    Co-authored-by: PatBCCA <PatBCCA@users.noreply.github.com>

--- capstone/mcewens/models.py
+++ capstone/mcewens/models.py
@@ -1,13 +1,110 @@
 from django.db import models
 
-# Create your models here.
 
 # one model - menu item
-# Name
-# Category - Lunch/ Dinner / Desert / Wine
-# Possibility of making exclusive toggle for ease of use? If Lunch, not WIne? Research?
-# Description
-# Price
-# Price percentage option?
-# Current item?
-# Submit button for forms
+class MenuItem(models.Model):
+    # Name - check
+    name = models.CharField(max_length=50)
+
+    # Category - Lunch/ Dinner / Desert / Wine - check
+    CATEGORY_CHOICES = (
+        ("Appetizer", "Appetizer"),
+        ("Lunch", "Lunch"),
+        ("Dinner", "Dinner"),
+        ("Dessert", "Dessert"),
+        ("Wine", "Wine"),
+    )
+
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=50)
+    # Possibility of making exclusive toggle for ease of use? If Lunch, not WIne? Research?
+
+    # Description - check
+    description = models.CharField(max_length=50, blank=True)
+
+    # Price - check
+    price = models.FloatField()
+    # Price percentage option?
+
+    # Current item? check
+    current = models.IntegerField(default=1)
+
+
+# Constructor - default values left blank
+def create_menu_item(name, description, price):
+    item = MenuItem(
+        name=name.title(),
+        description=description,
+        price=price,
+    )
+    item.save()
+    return item
+
+
+def get_current_items():
+    return MenuItem.objects.filter(current=1)
+
+
+def get_menu_items_by_category(cat):
+    return MenuItem.objects.filter(category=cat)
+
+
+def get_menu_items_by_name(name):
+    try:
+        return MenuItem.objects.get(name=name.title())
+    except:
+        return "No results found!"
+
+
+def get_current_by_category(cat):
+    return MenuItem.objects.filter(category=cat, current=1)
+
+
+def set_current_status(item_id, status):
+    try:
+        item = MenuItem.objects.get(id=item_id)
+        item.current = status
+        item.save()
+        return item
+    except:
+        raise ValueError("Item not found in database!")
+
+
+# Should be able to change the category
+def set_category(item_id, cat):
+    try:
+        item = MenuItem.objects.get(id=item_id)
+        item.category = cat
+        item.save()
+        return item
+    except:
+        raise ValueError("Item not found in database!")
+
+
+def set_menu_item_name(item_id, new_n):
+    try:
+        item = MenuItem.objects.get(id=item_id)
+        item.name = new_n
+        item.save()
+        return item
+    except:
+        raise ValueError("Item not found in database!")
+
+
+def set_menu_item_description(item_id, new_d):
+    try:
+        item = MenuItem.objects.get(id=item_id)
+        item.description = new_d
+        item.save()
+        return item
+    except:
+        raise ValueError("Item not found in database!")
+
+
+def set_menu_item_price(item_id, new_p):
+    try:
+        item = MenuItem.objects.get(id=item_id)
+        item.price = new_p
+        item.save()
+        return item
+    except:
+        raise ValueError("Item not found in database!")
--- capstone/mcewens/tests.py
+++ capstone/mcewens/tests.py
@@ -1,3 +1,96 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+
+class Test_MenuItem(TestCase):
+    def setUp(self) -> None:
+        self.item1 = models.create_menu_item(
+            "test1",
+            "This is a dish",
+            50.00,
+        )
+        self.item2 = models.create_menu_item(
+            "Test2",
+            "This is a dish",
+            50.00,
+        )
+        self.item3 = models.create_menu_item(
+            "Test3",
+            "This is a dish",
+            50.00,
+        )
+
+    def test_create_menu_item(self):
+        assert self.item1.id == 1
+        assert self.item1.name == "Test1"
+        assert self.item1.description == "This is a dish"
+        assert self.item1.price == 50.00
+        assert self.item1.current == 1
+        assert self.item1.category == ""
+
+    def test_current_items(self):
+        # Default to current
+        assert len(models.get_current_items()) == 3
+
+        # Can we change status?
+        models.set_current_status(self.item2.id, 0)
+        self.item2.refresh_from_db()
+        assert self.item2.current == 0
+        assert len(models.get_current_items()) == 2
+
+        # Can we change status??
+        models.set_current_status(self.item1.id, 0)
+        models.set_current_status(self.item2.id, 1)
+        self.item1.refresh_from_db()
+        self.item2.refresh_from_db()
+        assert self.item1.current == 0
+        assert self.item2.current == 1
+        assert len(models.get_current_items()) == 2
+
+        # Can we change everything's status?
+        models.set_current_status(self.item2.id, 0)
+        models.set_current_status(self.item3.id, 0)
+        self.item2.refresh_from_db()
+        self.item3.refresh_from_db()
+        assert self.item2.current == 0
+        assert self.item3.current == 0
+        assert len(models.get_current_items()) == 0
+
+    def test_category_for_items(self):
+        # Should default to unselected
+        assert len(models.get_menu_items_by_category("Wine")) == 0
+
+        models.set_category(self.item1.id, "Wine")
+        self.item1.refresh_from_db()
+        assert self.item1.category == "Wine"
+        assert len(models.get_menu_items_by_category("Wine")) == 1
+
+    def test_set_name(self):
+        assert self.item1.name == "Test1"
+        models.set_menu_item_name(self.item1.id, "New")
+        self.item1.refresh_from_db()
+        assert self.item1.name == "New"
+
+    def test_set_price(self):
+        assert self.item2.price == 50
+        models.set_menu_item_price(self.item2.id, 100)
+        self.item2.refresh_from_db()
+        assert self.item2.price == 100
+
+    def test_set_description(self):
+        assert self.item3.description == "This is a dish"
+        models.set_menu_item_description(self.item3.id, "I have altered the dish")
+        self.item3.refresh_from_db()
+        assert self.item3.description == "I have altered the dish"
+
+    def test_get_by_name(self):
+        assert self.item3.name == "Test3"
+        assert models.get_menu_items_by_name("test3") == self.item3
+        assert models.get_menu_items_by_name("test4") == "No results found!"
+
+    def test_get_current_by_cat(self):
+        assert self.item1.category == ""
+        result = models.get_current_by_category("")
+        assert len(result) == 3
+        result = models.get_current_by_category("Wine")
+        assert len(result) == 0
