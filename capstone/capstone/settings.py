fbe21f92dd78f5d677f145ed489a125f1d4edf28
JacobPadgett <jepadgett95@gmail.com>
2023-08-12 14:23:33 -0500

    password reset functionality complete

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -132,3 +132,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/restaurant_admin/"
 LOGOUT_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "mcewenswebsite@gmail.com"
+EMAIL_HOST_PASSWORD = "neyhkfpclqakilpc"
